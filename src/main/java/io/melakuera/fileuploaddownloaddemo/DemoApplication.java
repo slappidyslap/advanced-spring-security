8a12620175ae8542008c96fefb1eac830de78e6c
Melakuera <musabaeveldiar@gmail.com>
2022-05-21 17:36:54 +0600

    добавлены webjar

--- src/main/java/io/melakuera/fileuploaddownloaddemo/DemoApplication.java
+++ src/main/java/io/melakuera/fileuploaddownloaddemo/DemoApplication.java
@@ -2,14 +2,24 @@ package io.melakuera.fileuploaddownloaddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class DemoApplication {
+@Configuration
+public class DemoApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+				.addResourceHandler("/webjars/**")
+				.addResourceLocations("/webjars/").resourceChain(false);
+	}
 }
