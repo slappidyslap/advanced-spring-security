3815b81947b86782cea45e57583e7a0eb83cbe66
Melakuera <musabaeveldiar@gmail.com>
2022-05-23 17:43:19 +0600

    чекпоинт

--- src/main/java/io/melakuera/fileuploaddownloaddemo/DemoApplication.java
+++ src/main/java/io/melakuera/fileuploaddownloaddemo/DemoApplication.java
@@ -3,8 +3,7 @@ package io.melakuera.fileuploaddownloaddemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
--- src/main/java/io/melakuera/fileuploaddownloaddemo/FileStorageService.java
+++ src/main/java/io/melakuera/fileuploaddownloaddemo/FileStorageService.java
@@ -18,9 +18,7 @@ import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
